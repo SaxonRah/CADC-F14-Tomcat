b0ebc536e1fe9ebb5e8725ee9df9bcfb04dca150
Robert Valentine <paraboliclabs@gmail.com>
2024-01-18 00:57:36 -0500

    Create ParallelDivider.v
    20-bit parallel divider

---
+++ ParallelDivider.v
@@ -0,0 +1,34 @@
+module ParallelDivider (
+  input wire [19:0] dividend,
+  input wire [19:0] divisor,
+  output reg [19:0] quotient,
+  output reg [19:0] remainder
+);
+
+always @(dividend or divisor) begin
+  // Initialize vars
+  reg [19:0] temp_dividend;
+  reg [19:0] temp_quotient;
+  reg [4:0] count;
+
+  // Reset vars
+  temp_dividend = dividend;
+  temp_quotient = 0;
+  count = 20'd0;
+
+  // Division loop
+  while (count < 20'd20) begin
+    if (temp_dividend >= divisor) begin
+      temp_dividend = temp_dividend - divisor;
+      temp_quotient[count] = 1;
+    end
+    temp_dividend = temp_dividend << 1;
+    count = count + 1;
+  end
+
+  // Assign outputs
+  quotient = temp_quotient;
+  remainder = temp_dividend;
+end
+
+endmodule
