72633a0ad9a6a7b4efd77798734aaba75004982f
Robert Valentine <paraboliclabs@gmail.com>
2024-01-18 00:55:34 -0500

    Create Multiplier.v
    20-bit multiplier

---
+++ Multiplier.v
@@ -0,0 +1,12 @@
+module Multiplier(
+  input wire [19:0] operand_a,
+  input wire [19:0] operand_b,
+  output reg [39:0] result
+);
+
+always @(operand_a or operand_b) begin
+  // Multiply operands and store the result
+  result = operand_a * operand_b;
+end
+
+endmodule
