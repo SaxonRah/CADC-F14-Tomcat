e8ff62f7e56b69d2e65e51e997746ccef8d1e291
Robert Valentine <paraboliclabs@gmail.com>
2026-03-16 11:26:01 -0400

    Refactor PMU module for cleaner syntax

--- PMU.v
+++ PMU.v
@@ -1,12 +1,11 @@
-module PMU(
-  input wire [19:0] operand_a,
-  input wire [19:0] operand_b,
-  output reg [39:0] result
+module PMU (
+    input  wire [19:0] operand_a,
+    input  wire [19:0] operand_b,
+    output reg  [39:0] result
 );
 
-always @(operand_a or operand_b) begin
-  // Multiply operands and store the result
-  result = operand_a * operand_b;
-end
+    always @(*) begin
+        result = operand_a * operand_b;
+    end
 
 endmodule
