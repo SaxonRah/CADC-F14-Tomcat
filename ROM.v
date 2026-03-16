7ce2c152b0f795c8cbe783e7c751bd2702f8a7b8
Robert Valentine <paraboliclabs@gmail.com>
2026-03-16 11:36:21 -0400

    Refactor ROM module to use combinational output

--- ROM.v
+++ ROM.v
@@ -1,24 +1,14 @@
 module ROM (
-    input  wire        clk,
-    input  wire        rst_n,
     input  wire [6:0]  addr,
-    output reg  [19:0] data_out
+    output wire [19:0] data_out
 );
 
     reg [19:0] memory [0:127];
 
-    // Load contents from file
-    // populate rom_data.mem with patterns
     initial begin
-        $readmemb("rom_data.mem", memory);
+        $readmemh("program.mem", memory);
     end
 
-    always @(posedge clk or negedge rst_n) begin
-        if (~rst_n) begin
-            data_out <= 20'b0;
-        end else begin
-            data_out <= memory[addr];
-        end
-    end
+    assign data_out = memory[addr];
 
 endmodule
